7262684db691b37ba1ea1217968c563435bfa185
Nath <nathaniel@aims.ac.za>
2022-01-28 19:36:59 +0000

    template function definition with normal args

--- conversion/template_exs/funcTemplateExample.cpp
+++ conversion/template_exs/funcTemplateExample.cpp
@@ -6,6 +6,13 @@ T addNumbers(T aNumber, T bNumber)
 {
     return (aNumber < bNumber) ? aNumber : bNumber;
 }
+// template and non-template parameters
+template<typename U>
+U nonAndTemplateParameters(U jNumber, float kNumber)
+{
+    // std::cout << "second parameter: " << kNumber <<'\n';
+    return (jNumber < kNumber) ? jNumber : kNumber;
+}
 
 int main()
 {
@@ -14,10 +21,12 @@ int main()
     int bNumber {11};
     std::cout << "Adding both integers: " << addNumbers<int>(aNumber, bNumber) << '\n';
     // Declare floating parameters
-    float fNumber{2.003};
+    float fNumber{2.003f};
     float  gNumber{1.111};
     std::cout << "adding floating point: " << addNumbers<float>(fNumber, gNumber) <<'\n';
     std::cout << "Calling with normal functions " << addNumbers(fNumber, gNumber) <<'\n';
+    std::cout << "----------------------calling template and non-template parameters --------------------\n";
+    std::cout << "Non-template and template parameters: " << nonAndTemplateParameters(aNumber, fNumber) <<'\n';
 
     return 0;
 }
