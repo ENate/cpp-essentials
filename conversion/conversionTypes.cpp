d9c070f4e3d696c48e323f02606a3f4c08abe06d
Nath <nathaniel@aims.ac.za>
2022-01-28 15:47:22 +0000

    Added conversion examples

--- conversion/conversionTypes.cpp
+++ conversion/conversionTypes.cpp
@@ -2,6 +2,7 @@ int additionNumber(int number) {
     return number + number * 2;
 }
 
+#include <iomanip>
 #include <iostream>
 
 
@@ -12,5 +13,8 @@ int main() {
     std::cout << "The solution via int conversion is: " << additionNumber(dNum) << '\n';
     // Use static cast to avoid warning messages
     std::cout << "The solution via static cast is: " << additionNumber(static_cast<int>(dNum)) << '\n';
+    // For floating with loss of precision for converting from large to small values
+    float f = 0.123456789;
+    std::cout << "The solution from large to small is: " << std::setprecision(9) << f << '\n';
     return 0;
 }
