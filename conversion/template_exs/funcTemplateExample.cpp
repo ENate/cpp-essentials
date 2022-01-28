01b2bf36b93b2e94b886d66b3b6c71d156510163
Nath <nathaniel@aims.ac.za>
2022-01-28 19:18:19 +0000

    template function examples

---
+++ conversion/template_exs/funcTemplateExample.cpp
@@ -0,0 +1,21 @@
+// Start with a template parameter  declaration
+#include <iostream>
+
+template<typename T>
+T addNumbers(T aNumber, T bNumber)
+{
+    return (aNumber < bNumber) ? aNumber : bNumber;
+}
+
+int main()
+{
+    // Initialize the parameters
+    int aNumber {23};
+    int bNumber {11};
+    std::cout << "Adding both integers" << addNumbers<int>(aNumber, bNumber) << '\n';
+    // Declare floating parameters
+    float fNumber{2.003};
+    float  gNumber{1.111};
+    std::cout << "adding floating point" << addNumbers<float>(fNumber, gNumber) <<'\n';
+    return 0;
+}
